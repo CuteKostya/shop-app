0e21dc89609e61239458693e5281ad4e7f5901ea
CuteKostya <79268579+CuteKostya@users.noreply.github.com>
2024-03-30 18:08:42 +0400

    headings

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Order;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class OrdersExport implements FromQuery
+class OrdersExport implements FromQuery, WithHeadings
 {
     use Exportable;
 
@@ -21,5 +22,15 @@ class OrdersExport implements FromQuery
     public function query()
     {
         return Order::query()->where('user_id', $this->id);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Order_id',
+            'User_id',
+            'create_at',
+            'update_at',
+        ];
     }
 }
